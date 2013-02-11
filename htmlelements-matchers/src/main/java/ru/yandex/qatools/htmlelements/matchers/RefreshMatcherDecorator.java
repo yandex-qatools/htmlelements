8a31233ca0abeba51874f17b48a654e1909ddc1b
MerkushevKirill <lanwen@yandex-team.ru>
2013-02-12 01:29:14 +0400

    Pre(Post)-refresh with WebDriver matcher decorator

---
+++ htmlelements-matchers/src/main/java/ru/yandex/qatools/htmlelements/matchers/RefreshMatcherDecorator.java
@@ -0,0 +1,71 @@
+package ru.yandex.qatools.htmlelements.matchers;
+
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lanwen
+ * Date: 08.02.13
+ * Time: 17:37
+ */
+public class RefreshMatcherDecorator<T> extends TypeSafeMatcher<T> {
+
+    private WebDriver driver;
+    private Matcher<? extends T> matcher;
+
+    private boolean isPrerefresh = true;
+
+    @Override
+    protected boolean matchesSafely(T item) {
+        if (isPrerefresh) {
+            driver.navigate().refresh();
+        }
+        try {
+            return matcher.matches(item);
+        } finally {
+            if(!isPrerefresh) {
+                driver.navigate().refresh();
+            }
+        }
+    }
+
+    public RefreshMatcherDecorator(Matcher<? extends T> matcher, WebDriver driver) {
+        this.driver = driver;
+        this.matcher = matcher;
+    }
+
+    private RefreshMatcherDecorator<T> postrefresh() {
+        isPrerefresh = false;
+        return this;
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText(" (after refresh) ").appendDescriptionOf(matcher);
+
+    }
+
+
+    @Override
+    protected void describeMismatchSafely(T item, Description mismatchDescription) {
+        matcher.describeMismatch(item, mismatchDescription);
+    }
+
+
+    @Factory
+    public static <T> Matcher<T> withPrerefresh(Matcher<? extends T> matcher, WebDriver driver) {
+        return new RefreshMatcherDecorator<T>(matcher, driver);
+    }
+
+
+    @Factory
+    public static <T> Matcher<T> withPostrefresh(Matcher<? extends T> matcher, WebDriver driver) {
+        return new RefreshMatcherDecorator<T>(matcher, driver).postrefresh();
+    }
+
+
+}
---
+++ htmlelements-matchers/src/test/java/ru/yandex/qatools/htmlelements/matchers/RefreshMatcherTest.java
@@ -0,0 +1,103 @@
+package ru.yandex.qatools.htmlelements.matchers;
+
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.openqa.selenium.WebDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.openqa.selenium.WebDriver.Navigation;
+import static ru.yandex.qatools.htmlelements.matchers.RefreshMatcherDecorator.withPostrefresh;
+import static ru.yandex.qatools.htmlelements.matchers.RefreshMatcherDecorator.withPrerefresh;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lanwen
+ * Date: 12.02.13
+ * Time: 0:08
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class RefreshMatcherTest {
+
+    public static final boolean THE_OBJECT = true;
+    public static final boolean NOT_SAME_OBJECT = false;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+
+    @Mock private WebDriver driver;
+    @Mock private Navigation navigation;
+    @Mock private Matcher<Object> matcher;
+
+    @Test
+    public void isPrerefreshingMatching() {
+        thrown.expect(AssertionError.class);
+
+        when(driver.navigate()).thenReturn(navigation);
+        assertThat(THE_OBJECT, withPrerefresh(is(NOT_SAME_OBJECT), driver));
+    }
+
+    @Test
+    public void isPostrefreshingMatching() {
+        thrown.expect(AssertionError.class);
+
+        when(driver.navigate()).thenReturn(navigation);
+        assertThat(THE_OBJECT, withPostrefresh(is(NOT_SAME_OBJECT), driver));
+    }
+
+
+    @Test
+    public void isPrerefreshingRefreshesDriver() {
+        when(driver.navigate()).thenReturn(navigation);
+        assertThat(THE_OBJECT, withPrerefresh(is(THE_OBJECT), driver));
+
+        verify(navigation, times(1)).refresh();
+    }
+
+
+    @Test
+    public void isPostrefreshingRefreshesDriver() {
+        when(driver.navigate()).thenReturn(navigation);
+        assertThat(THE_OBJECT, withPostrefresh(is(THE_OBJECT), driver));
+
+        verify(navigation, times(1)).refresh();
+    }
+
+
+    @Test
+    public void isPostrefreshRefreshAfterMatching() {
+        when(driver.navigate()).thenReturn(navigation);
+        when(matcher.matches(THE_OBJECT)).thenReturn(THE_OBJECT);
+
+
+        assertThat(THE_OBJECT, withPostrefresh(matcher, driver));
+
+        InOrder inOrder = inOrder(matcher, navigation);
+
+        inOrder.verify(matcher).matches(THE_OBJECT);
+        inOrder.verify(navigation).refresh();
+    }
+
+    @Test
+    public void isPrerefreshRefreshBeforeMatching() {
+        when(driver.navigate()).thenReturn(navigation);
+        when(matcher.matches(THE_OBJECT)).thenReturn(THE_OBJECT);
+
+
+        assertThat(THE_OBJECT, withPrerefresh(matcher, driver));
+
+        InOrder inOrder = inOrder(matcher, navigation);
+
+        inOrder.verify(navigation).refresh();
+        inOrder.verify(matcher).matches(THE_OBJECT);
+    }
+
+}
