d595b86d79f471ce4c222b165949c96e56339f97
Alexander Tolmachev <starlight@yandex-team.ru>
2013-02-22 15:29:32 +0400

    Edited Suggest sample to make in typified element

--- htmlelements-samples/src/main/java/ru/yandex/qatools/htmlelements/samples/elements/Suggest.java
+++ htmlelements-samples/src/main/java/ru/yandex/qatools/htmlelements/samples/elements/Suggest.java
@@ -1,21 +1,39 @@
 package ru.yandex.qatools.htmlelements.samples.elements;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import ru.yandex.qatools.htmlelements.element.HtmlElement;
+import ru.yandex.qatools.htmlelements.element.TypifiedElement;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * User: eroshenkoam
  * Date: 2/7/13, 3:13 PM
  */
-public class Suggest extends HtmlElement {
+public class Suggest extends TypifiedElement {
 
-    @FindBy(tagName = "li")
-    private List<WebElement> suggestTips;
+    public Suggest(WebElement wrappedElement) {
+        super(wrappedElement);
+    }
 
-    public List<WebElement> getTips() {
-        return suggestTips;
+    private List<WebElement> getItems() {
+        return getWrappedElement().findElements(By.xpath("//li"));
+    }
+
+    public void selectByIndex(int itemIndex) {
+        getItems().get(itemIndex).click();
+    }
+
+    public void selectByValue(String itemValue) {
+        for (WebElement item : getItems()) {
+            if (itemValue.equals(item.getText())) {
+                item.click();
+                return;
+            }
+        }
+        throw new NoSuchElementException();
     }
 }
