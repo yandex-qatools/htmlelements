61ea892075a39c1f335be7964d970038a48671a0
Alexander Tolmachev <starlight@yandex-team.ru>
2013-02-26 17:27:33 +0400

    Added AccountingSuperclassAnnotationTest

---
+++ htmlelements-java/src/test/java/ru/yandex/qatools/htmlelements/testelements/NotAnnotatedExtendedSearchArrow.java
@@ -0,0 +1,9 @@
+package ru.yandex.qatools.htmlelements.testelements;
+
+/**
+ * @author Alexander Tolmachev starlight@yandex-team.ru
+ *         Date: 26.02.13
+ */
+public class NotAnnotatedExtendedSearchArrow extends SearchArrow {
+    //
+}
