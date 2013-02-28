5052c130f0657bcbe697d3e151fb4edbb8480e38
Alexander Tolmachev <starlight@yandex-team.ru>
2013-02-28 19:05:33 +0400

    Added Action interface

---
+++ htmlelements-matchers/src/main/java/ru/yandex/qatools/htmlelements/matchers/decorators/Action.java
@@ -0,0 +1,9 @@
+package ru.yandex.qatools.htmlelements.matchers.decorators;
+
+/**
+ * @author Alexander Tolmachev starlight@yandex-team.ru
+ *         Date: 28.02.13
+ */
+public interface Action {
+    void perform();
+}
