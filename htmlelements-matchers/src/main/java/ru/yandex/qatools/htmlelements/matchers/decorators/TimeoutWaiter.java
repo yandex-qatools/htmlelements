655dda9c98b7a2c6beb97b4c6c22553733a25626
Alexander Tolmachev <starlight@yandex-team.ru>
2013-03-01 18:37:38 +0400

    Added abstract Waiter class and TimeoutWaiter

---
+++ htmlelements-matchers/src/main/java/ru/yandex/qatools/htmlelements/matchers/decorators/TimeoutWaiter.java
@@ -0,0 +1,27 @@
+package ru.yandex.qatools.htmlelements.matchers.decorators;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+/**
+ * @author Alexander Tolmachev starlight@yandex-team.ru
+ *         Date: 01.03.13
+ */
+public class TimeoutWaiter extends Waiter {
+    private static final long DEFAULT_TIMEOUT = SECONDS.toMillis(30);
+
+    private final long timeout;
+
+    public TimeoutWaiter() {
+        this.timeout = DEFAULT_TIMEOUT;
+    }
+
+    public TimeoutWaiter(long timeout) {
+        this.timeout = timeout;
+    }
+
+    @Override
+    public boolean shouldStopWaiting() {
+        long currentTime = System.currentTimeMillis();
+        return currentTime >= getStartTime() + timeout;
+    }
+}
