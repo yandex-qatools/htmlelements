b25a7922c1ddc4c827adc1ad26ae16f095985371
Andrew Sumner <andrew.sumner@assurity.co.nz>
2016-03-04 10:30:04 +1300

    Fix sonar issues

--- htmlelements-java/src/main/java/ru/yandex/qatools/htmlelements/loader/decorator/WebDriverAware.java
+++ htmlelements-java/src/main/java/ru/yandex/qatools/htmlelements/loader/decorator/WebDriverAware.java
@@ -3,5 +3,5 @@ package ru.yandex.qatools.htmlelements.loader.decorator;
 import org.openqa.selenium.WebDriver;
 
 public interface WebDriverAware {
-	public void setWebDriver(WebDriver driver);
+    public void setWebDriver(WebDriver driver);
 }
--- htmlelements-java/src/main/java/ru/yandex/qatools/htmlelements/loader/decorator/WebDriverAwareDecorator.java
+++ htmlelements-java/src/main/java/ru/yandex/qatools/htmlelements/loader/decorator/WebDriverAwareDecorator.java
@@ -10,35 +10,35 @@ import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementDecorator;
 import ru.yandex.qatools.htmlelements.pagefactory.CustomElementLocatorFactory;
 
 public class WebDriverAwareDecorator extends HtmlElementDecorator {
-	private WebDriver driver;
-	
-	public WebDriverAwareDecorator(CustomElementLocatorFactory factory, WebDriver driver) {
-		super(factory);
-		
-		this.driver = driver;
-	}
+    private WebDriver driver;
+    
+    public WebDriverAwareDecorator(CustomElementLocatorFactory factory, WebDriver driver) {
+        super(factory);
+        
+        this.driver = driver;
+    }
 
-	@Override
-	protected <T extends HtmlElement> T decorateHtmlElement(ClassLoader loader, Field field) {
-		T element = super.decorateHtmlElement(loader, field);
-		
-		if (element instanceof WebDriverAware) {
-			((WebDriverAware)element).setWebDriver(driver);
-		}		
-		
-		return element;
-	}
-	
-	@Override
-	protected <T extends HtmlElement> List<T> decorateHtmlElementList(ClassLoader loader, Field field) {
-		List<T> elements = super.decorateHtmlElementList(loader, field);
-		
-		if (elements.size() > 0 && elements.get(0) instanceof WebDriverAware) {
-			for (T element : elements) {
-				((WebDriverAware)element).setWebDriver(driver);
-			}
-		}
+    @Override
+    protected <T extends HtmlElement> T decorateHtmlElement(ClassLoader loader, Field field) {
+        T element = super.decorateHtmlElement(loader, field);
+        
+        if (element instanceof WebDriverAware) {
+            ((WebDriverAware)element).setWebDriver(driver);
+        }        
+        
+        return element;
+    }
+    
+    @Override
+    protected <T extends HtmlElement> List<T> decorateHtmlElementList(ClassLoader loader, Field field) {
+        List<T> elements = super.decorateHtmlElementList(loader, field);
+        
+        if (!elements.isEmpty() && elements.get(0) instanceof WebDriverAware) {
+            for (T element : elements) {
+                ((WebDriverAware)element).setWebDriver(driver);
+            }
+        }
 
-		return elements;
-	}
+        return elements;
+    }
 }
