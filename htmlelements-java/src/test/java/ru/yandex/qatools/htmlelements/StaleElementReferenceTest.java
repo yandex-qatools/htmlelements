40c5bcee06f6a411816414a3ac5bec8564380125
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2017-01-20 11:42:11 +0700

    Expect StaleElementReferenceException on getText

--- htmlelements-java/src/test/java/ru/yandex/qatools/htmlelements/StaleElementReferenceTest.java
+++ htmlelements-java/src/test/java/ru/yandex/qatools/htmlelements/StaleElementReferenceTest.java
@@ -1,6 +1,9 @@
 package ru.yandex.qatools.htmlelements;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.openqa.selenium.StaleElementReferenceException;
 import ru.yandex.qatools.htmlelements.testpages.StaleElementReferencePage;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -11,9 +14,13 @@ import static org.junit.Assert.assertThat;
  */
 public class StaleElementReferenceTest {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
     public void testOutput() {
         StaleElementReferencePage staleElementReferencePage = new StaleElementReferencePage();
+        thrown.expect(StaleElementReferenceException.class);
         assertThat(staleElementReferencePage.getElement().getText(), equalTo(StaleElementReferencePage.ELEMENT_TEXT));
     }
 
